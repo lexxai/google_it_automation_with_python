d59754812092c26f9f2d9614bde425f4bd790020
lexxai <lexxai@users.noreply.github.com>
2023-09-03 23:37:24 +0300

    Update dailysync.py

--- 04_Troubleshooting and Debugging Techniques/Module 2_ Slowness/04_Module 2 Review/scripts/scripts/dailysync.py
+++ 04_Troubleshooting and Debugging Techniques/Module 2_ Slowness/04_Module 2 Review/scripts/scripts/dailysync.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 
+import multiprocessing 
 import subprocess
-import multiprocessing
 import os
-from multiprocessing import Pool
 
 home_path = os.path.expanduser('~')
-src = home_path +  "/data/prod/"
-dest = home_path +  "/data/prod_backup/"
+# home_path = os.path.relpath("..")
+
+src = "data/prod"
+dest = "data/prod_backup"
+
+src_path = os.path.join(home_path, src)
+
+def run(src, dest):
+  subprocess.call(["rsync", "-arq", f"{src}/", dest])
+  # print(f'subprocess.call(["rsync", "-arq", {src}/, {dest}])')
+
 
 if __name__ == "__main__":
-    pool = Pool(multiprocessing.cpu_count())
-    pool.apply(subprocess.call, args=(["rsync", "-arq", src, dest],))
+
+  print(src_path, dest)
+  for root, dirs, files in os.walk(src_path):
+    # print(root, dirs, files)
+    src_p = root
+    dest_p = src_p.replace(src, dest)
+    p = multiprocessing.Pool(multiprocessing.cpu_count())
+    p.apply(run, (src_p, dest_p))
